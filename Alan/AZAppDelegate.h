933b96e2d41edba26e0cf53e8e603aef8f480a12
Alan Zeino <alan.zeino@gmail.com>
2013-05-02 20:08:33 +1000

    Initial Commit

---
+++ Alan/AZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AZAppDelegate.h
+//  Alan
+//
+//  Created by Alan on 2/05/13.
+//  Copyright (c) 2013 Alan Zeino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Alan/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Alan
+//
+//  Created by Alan on 2/05/13.
+//  Copyright (c) 2013 Alan Zeino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AZAppDelegate class]));
+    }
+}
